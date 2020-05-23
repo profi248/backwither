ff7310992589ebc716242a6b2f3c713dad83ac03
David Košťál <kostal.david8@gmail.com>
2020-05-24 01:08:27 +0200

    add last resort catch to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <exception>
+
 #include "FilesystemBrowser.h"
 #include "SQLiteConfigProvider.h"
 #include "TerminalUserInterface.h"
@@ -5,7 +8,11 @@
 int main (int argc, char** argv) {
     TerminalUserInterface tui;
 
-    return tui.StartInterface(argc, argv);
+    try {
+        return tui.StartInterface(argc, argv);
+    } catch (std::exception const & e) {
+        std::cerr << "Unexpected fatal error: " << e.what() << std::endl;
+    }
 
     // Directory* root = FilesystemBrowser::BrowseFolderRecursive("/var/home/david/ProgTest/PA2/kostada2/examples");
 }
